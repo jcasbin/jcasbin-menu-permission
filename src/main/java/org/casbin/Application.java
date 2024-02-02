af3ae0c90a23b3027e33eab429c32ac5834ce754
Xu Zerui <154111660+amikecoXu@users.noreply.github.com>
2024-02-02 23:04:29 +0900

    feat: Improve user friendliness
    feat: Improve user friendliness

--- src/main/java/org/casbin/Application.java
+++ src/main/java/org/casbin/Application.java
@@ -14,14 +14,37 @@
 
 package org.casbin;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+import org.springframework.util.StringUtils;
+
+import java.lang.management.ManagementFactory;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
+@Slf4j
 public class Application {
-
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext application = SpringApplication.run(Application.class, args);
+        Environment env = application.getEnvironment();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String port = env.getProperty("server.port");
+        String path = env.getProperty("server.servlet.context-path");
+        if (StringUtils.isEmpty(path)) {
+            path = "";
+        }
+        log.info("\n----------------------------------------------------------\n\t" +
+                "Application  is running! Access URLs:\n\t" +
+                "Local access website: \t\thttp://localhost:" + port + path + "\n\t" +
+                "External access website: \thttp://" + ip + ":" + port + path + "\n\t" +
+                "----------------------------------------------------------");
+        String jvmName = ManagementFactory.getRuntimeMXBean().getName();
+        log.info("Current project progress number：" + jvmName.split("@")[0]);
     }
 
 }
+
